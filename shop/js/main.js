400ef671c29f309d2527bb32e0718902d2ed19a2
DevStardi <luca@thepixelhead.co>
2021-07-15 12:41:32 +0200

    small tweaks

--- shop/js/main.js
+++ shop/js/main.js
@@ -102,5 +102,21 @@ $(document).ready(function() {
     $('body').removeClass('popup_active');
   });
 
-});
 
+  $('span.size').on('click', function(){
+    var $size = this.getAttribute('size');
+
+    $('p.size_selector span').removeClass('selected')
+    $(this).addClass('selected')
+
+    var email = 'shop@thepixelhead.co';
+    var subject = encodeURIComponent('Holo Shirt white blue');
+    var body = encodeURIComponent('Hallo,\nich würde gerne das Shirt in Größe '+$size+' bestellen');
+    $('a.button.order').attr('href', 'mailto:'+email+'?subject='+subject+'&body='+body);
+    
+    $('a.button.order').removeClass('no_click')
+
+  });
+
+
+});
