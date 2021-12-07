c54bd8dd2305cd0662d88bcd0cd5a6b886e27377
DevStardi <luca@thepixelhead.co>
2021-12-07 22:33:58 +0100

    added build (not perfect yet) to /web

--- dev/gulpfile.js
+++ dev/gulpfile.js
@@ -1,21 +1,23 @@
-const { src, dest, watch} = require('gulp');
+const { src, dest, watch, series} = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const browserSync = require('browser-sync').create();
+
+const prefix = require('gulp-autoprefixer');
+const minify = require('gulp-clean-css');
+
 const terser = require('gulp-terser');
 
 
 function style() { 
     return src('./scss/**/*.scss') 
     .pipe(sass().on('error', sass.logError))
-    .pipe(dest('./dist'))
+    .pipe(dest('./tmp'))
     .pipe(browserSync.stream());
 }
-
-// minify js
 function jsmin(){
     return src('./scss/**/*.js') // change to your source directory
     .pipe(terser())
-    .pipe(dest('./dist')) // change to your final/public directory
+    .pipe(dest('./tmp')) // change to your final/public directory
     .pipe(browserSync.stream());
 }
 
@@ -30,6 +32,27 @@ function dev() {
     watch('./**/*.html').on('change', browserSync.reload);
 }
 
+function styleBuild() { 
+    return src('./scss/**/*.scss') 
+    .pipe(sass())
+    .pipe(prefix('last 2 versions'))
+    .pipe(minify())
+    .pipe(dest('../web/styles'))
+}
+function jsminBuild(){
+    return src('./scss/**/*.js') // change to your source directory
+    .pipe(terser())
+    .pipe(dest('../web/scripts')) // change to your final/public directory
+}
+
+const build = series(
+    styleBuild,
+    jsminBuild
+);
+
+
 exports.style = style;
 exports.jsmin = jsmin;
 exports.dev = dev;
+
+exports.build = build;
