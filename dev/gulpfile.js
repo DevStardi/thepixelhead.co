bcb43683faf3a8a3c265fb0cf669374bc5bffa4f
DevStardi <luca@thepixelhead.co>
2021-12-08 23:31:05 +0100

    optimized gulp setup / dev & build setup - small style tweaks

--- dev/gulpfile.js
+++ dev/gulpfile.js
@@ -1,58 +1,64 @@
-const { src, dest, watch, series} = require('gulp');
+const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const minify = require('gulp-clean-css');
+const terser = require('gulp-terser');
+const prefix = require('gulp-autoprefixer');
 const browserSync = require('browser-sync').create();
 
-const prefix = require('gulp-autoprefixer');
-const minify = require('gulp-clean-css');
 
-const terser = require('gulp-terser');
-
-
-function style() { 
+// SCSS Stuff
+function CSS() {
     return src('./scss/**/*.scss') 
     .pipe(sass().on('error', sass.logError))
+}
+function CSSstream() {
+    return(CSS())
     .pipe(dest('./tmp'))
     .pipe(browserSync.stream());
 }
-function jsmin(){
-    return src('./scss/**/*.js') // change to your source directory
-    .pipe(terser())
-    .pipe(dest('./tmp')) // change to your final/public directory
-    .pipe(browserSync.stream());
+function CSSbuild() {
+    return(CSS())
+    .pipe(prefix('last 2 versions'))
+    .pipe(minify())
+    .pipe(dest('../web/styles'));
 }
 
-function dev() {
+// JS stuff
+function jsmin(){
+    return src('./scss/**/*.js')
+    .pipe(terser());
+}
+function JSstream(){
+    return (jsmin())
+    .pipe(dest('./tmp'))
+    .pipe(browserSync.stream());
+}
+function JSbuild(){
+    return (jsmin())
+    .pipe(dest('../web/scripts'));
+}
+
+// Browser Sync stuff
+function sync() {
     browserSync.init({
         server: {
             baseDir: './'
         }
     });
-    watch('./scss/**/*.scss', style);
-    watch('./scss/**/*.js', jsmin);
+    watch('./scss/**/*.scss', CSSstream);
+    watch('./scss/**/*.js', JSstream);
     watch('./**/*.html').on('change', browserSync.reload);
 }
 
-function styleBuild() { 
-    return src('./scss/**/*.scss') 
-    .pipe(sass())
-    .pipe(prefix('last 2 versions'))
-    .pipe(minify())
-    .pipe(dest('../web/styles'))
-}
-function jsminBuild(){
-    return src('./scss/**/*.js') // change to your source directory
-    .pipe(terser())
-    .pipe(dest('../web/scripts')) // change to your final/public directory
-}
-
+const dev = series(
+    CSSstream,
+    JSstream,
+    sync
+)
 const build = series(
-    styleBuild,
-    jsminBuild
-);
+    CSSbuild,
+    JSbuild
+)
 
-
-exports.style = style;
-exports.jsmin = jsmin;
 exports.dev = dev;
-
 exports.build = build;
